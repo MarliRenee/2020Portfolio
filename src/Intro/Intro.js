61e092cb359664b80b7a72493b056f3e552d6827
Marli Renee <marlirenee@gmail.com>
2020-10-02 19:13:47 -0700

    responsive header

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -13,9 +13,6 @@ export default class Intro extends Component {
                         <button>Portfolio</button>
                     </div>
                     <img className="flowers" src="https://github.com/MarliRenee/2020Portfolio/blob/main/src/Assets/Card-01.png?raw=true" alt="a soft, peachy bunch of flowers, set just behind a steaming mug of tea"></img>
-                    <div className="IntroImage">
-                        
-                    </div>
                 </div>
             </div>
         )
