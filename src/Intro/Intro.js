b08ba2533d7801a92d7816c7fcea74b926f52609
Marli Renee <marlirenee@gmail.com>
2020-10-02 18:31:17 -0700

    asset changes

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -12,6 +12,10 @@ export default class Intro extends Component {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                         <button>Portfolio</button>
                     </div>
+                    <img className="flowers" src="https://github.com/MarliRenee/2020Portfolio/blob/main/src/Assets/Card-01.png?raw=true" alt="a soft, peachy bunch of flowers, set just behind a steaming mug of tea"></img>
+                    <div className="IntroImage">
+                        
+                    </div>
                 </div>
             </div>
         )
