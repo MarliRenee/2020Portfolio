1c2e7877ea701e83c4aea7d3d69a353a5b447225
Marli Renee <marlirenee@gmail.com>
2020-10-07 12:07:47 -0700

    working on case studies

--- src/About/About.js
+++ src/About/About.js
@@ -8,8 +8,8 @@ export default class About extends Component {
         return (
             <div className='About'>
                 <h2>About</h2>
-                <p>Marli Renee here. I'm a designer, developer & artist based in San Diego. I care about excellent, accessible education. My art, designs and multi-module courses have been used by government agencies, 
-                schools, and Fortune 500 companies. I started as a children's educational illlustrator, but curiosity eventually led me to UI/UX and full-stack web development. </p>
+                <p>Marli Renee here. I'm a designer, developer & artist based in San Diego. I care about excellent, accessible education. My designs have been used by government agencies, 
+                schools, and Fortune 500 companies.</p>
             </div>
         )
     }
--- src/Portfolio/CaseStudy1/CaseStudy1.js
+++ src/Portfolio/CaseStudy1/CaseStudy1.js
@@ -5,11 +5,15 @@ export default class CaseStudy1 extends Component {
 
     render () {
         return (
-            <div className="CaseStudy">
+            <div className='CaseStudy'>
                 <div className='Title&Intro'>
+                    <div className='Mockups'>
+                        <img src='https://github.com/MarliRenee/2020Portfolio/blob/main/src/Assets/CalSoap/CalSoapMockups.jpg?raw=true'/>
+                    </div>
                     <h2>Case Study Name</h2>
                     <h3>Subtitle</h3>
                     <p>Intro paragraph & assumptions</p>
+
                 </div>
                 <div className='Process'>
                     <h3>Process & Experience</h3>
