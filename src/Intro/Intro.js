6da07120c2004f4759d60b9378ef1058d848086d
Marli Renee <marlirenee@gmail.com>
2020-10-08 15:41:50 -0700

    case study 2 changes

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -8,8 +8,8 @@ export default class Intro extends Component {
             <div className='Intro'>
                 <div className="Inner">
                     <div className="InnerText">
-                        <h1>One Woman Army</h1>
-                        <p>I design, illustrate and code beautiful learning experiences. </p>
+                        <h1>Designer & Developer</h1>
+                        <p>I design and code learning experiences. </p>
                     </div>
                     <img className="flowers" src="https://github.com/MarliRenee/2020Portfolio/blob/main/src/Assets/Card-01.png?raw=true" alt="a soft, peachy bunch of flowers, set just behind a steaming mug of tea"></img>
                 </div>
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,13 +5,14 @@ export default class Navbar extends Component {
 
     render () {
         return (
+            
             <div className='Navbar'>
             <ul className="navbar">
                 <li className="Name">Marli Renee</li>
                 <li><a href='mailto:marlirenee@gmail.com'>Contact</a></li>
             </ul>
                
-            </div>
+            </div>   
         )
     }
 }
